58c720b0c79068bd8f0febeeb6d19a4c04f3741a
francisco <franciscoaguaslopes@gmail.com>
2022-12-18 18:43:00 +0000

    CrossOver

--- src/Individual.java
+++ src/Individual.java
@@ -6,6 +6,13 @@ public class Individual {
         this.chromossoma = chromossoma;
     }
 
+    public Individual (int l) {
+        this.chromossoma = new int[l];
+        for (int i = 0; i < l; i++) {
+            this.chromossoma[i] = -1;
+        }
+    }
+
     public Individual (int[] chromossoma, IProblem fitness) {
         this.chromossoma = chromossoma;
         this.fitness = fitness.fitness(this);
@@ -17,6 +24,63 @@ public class Individual {
 
     public int getFitness() {
         return this.fitness;
+    }
+
+
+
+    private int definido (Individual filho) {
+        for (int i = 0; i < filho.chromossoma.length; i++) {
+            if (filho.chromossoma[i] == -1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //procura o indice do allele do cromossma2 que esta no cromossoma1
+    public int searchAllele1 (int allele2, Individual p1) {
+        for (int i = 0; i < p1.chromossoma.length; i++) {
+            if (allele2 == p1.chromossoma[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void findCycle (Individual p1, Individual p2, int result, Individual filho) {
+        int allele1 = p1.chromossoma[result];
+        int i = result;
+
+        while (true) {
+            filho.chromossoma[i] = p1.chromossoma[i];
+            int allele2 = p2.chromossoma[i];
+
+            i = searchAllele1(allele2, p1);
+            if (allele1 == p2.chromossoma[i]) {
+                filho.chromossoma[i] = p1.chromossoma[i];
+                return;
+            }
+        }
+    }
+
+    public Individual cycleCrossover(Individual p2) {
+        int result; //result é o numero de elementos dentro do ciclo
+        int ciclos = 0; //numero de ciclos
+
+        int tamanho = this.chromossoma.length;
+
+        Individual filho = new Individual(tamanho);
+
+        while ((result = definido(filho)) != -1) {
+            if (ciclos % 2 == 0) {
+                findCycle(this, p2, result, filho);
+            }
+            else {
+                findCycle(p2, this, result, filho);
+            }
+            ciclos++;
+        }
+        return filho;
     }
 
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Population pop = new Population(6, new Fitness());
-        System.out.println(pop.getIndividuals().get(0));
+//        Population pop = new Population(6, new Fitness());
+//        System.out.println(pop.getIndividuals().get(0));
+        int []a1 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        Individual p1 = new Individual(a1, new Fitness());
+        int []a2 = {15,12,10,7,6,8,11,14,13,9,2,3,4,5,1};
+        Individual p2 = new Individual(a2, new Fitness());
+        Individual filho1 = p1.cycleCrossover(p2);
+        for (int i = 0; i < filho1.getChromossoma().length; i++) {
+            System.out.print(filho1.getChromossoma()[i] + " ");
+        }
+        System.out.println();
+        Individual filho2 = p2.cycleCrossover(p1);
+        for (int i = 0; i < filho2.getChromossoma().length; i++) {
+            System.out.print(filho2.getChromossoma()[i] + " ");
+        }
     }
 }

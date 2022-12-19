adeb5c56e73e4dd3150276fa09937c2c1918130d
francisco <franciscoaguaslopes@gmail.com>
2022-12-19 16:22:24 +0000

    CrossOver

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        Population pop = new Population(6, new Fitness());
 //        System.out.println(pop.getIndividuals().get(0));
-        int []a1 = {10,9,8,7,6,5,4,3,2,1,16,15,14,13,12,11};
-        Individual p1 = new Individual(a1, new Fitness());
-        int []a2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-        Individual p2 = new Individual(a2, new Fitness());
-        Individual filho1 = p1.cycleCrossover(p2);
-        for (int i = 0; i < filho1.getChromossoma().length; i++) {
-            System.out.print(filho1.getChromossoma()[i] + " ");
-        }
-        System.out.println();
-        Individual filho2 = p2.cycleCrossover(p1);
-        for (int i = 0; i < filho2.getChromossoma().length; i++) {
-            System.out.print(filho2.getChromossoma()[i] + " ");
-        }
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        SGA a = new SGA(l, new Fitness());
+        Individual b = a.solve(l);
+        System.out.println(b);
     }
 }
--- src/SGA.java
+++ src/SGA.java
@@ -4,27 +4,27 @@ public class SGA {
     Population populacao;
     IProblem fitness;
 
-    public SGA(int l, int n, IProblem fitness) {
-        this.populacao = new Population(l, n, fitness);
+    public SGA(int l, IProblem fitness) {
+        this.populacao = new Population(l, fitness);
         this.fitness = fitness;
     }
 
-    public boolean searchBoardWinner() {
+    public Individual searchBoardWinner() {
         for (Individual l : this.populacao.getIndividuals()) {
             if (l.getFitness() == 0) {
-                return true;
+                return l;
             }
         }
-        return false;
+        return null;
     }
 
     public Individual solve(int l) {
-        while (!searchBoardWinner()) {
+        Individual l1 = null;
+        while ((l1 = searchBoardWinner()) == null) {
             this.populacao = this.populacao.tournamentSelecNoReplacement(l, this.fitness);
             this.populacao = this.populacao.crossOver(this.fitness);
-            this.populacao = this.populacao.mutation();
+            this.populacao.mutation(this.fitness);
         }
-        populacao.getIndividuals().sort((s1, s2) -> (int) Math.signum(s1.getFitness() - s2.getFitness()));
-        return populacao.getIndividuals().get(0);
+        return l1;
     }
 }
